78c59d5ff8c5feb6d39c191d49b22cec280d2a51
AphroditeF <a.flora@outlook.com>
2023-08-16 17:21:19 +0300

    001 Class Car added

---
+++ Car.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecapCSharp310
+{
+    internal class Car
+    {
+        private string _name; //private field
+        private int _hp;
+        private string _color;
+
+        //Default Constructor
+        public Car() {
+            _name = "Car";
+            _hp = 0;
+            _color = "red";
+        }
+        //Partial Specification Constructor
+        public Car(string name, int hp = 0)
+        {
+            _name = name;
+            _hp = hp;
+            _color = "red";
+            Console.WriteLine(name + "was created!");
+        }
+        //Full Specification Constructor
+        public Car(string name, int hp, string color)
+        {
+            _name = name;
+            _hp = hp;
+            _color = color;
+            Console.WriteLine(name + "was created!");
+        }
+        public void Drive() {
+            Console.WriteLine(_name + " is driving");
+        }
+        public void Stop()
+        {
+            Console.WriteLine(_name + " stopped!");
+        }
+        public void Details() {
+            Console.WriteLine("The " + _color + " car " + _name + " has " + _hp + " hp.");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,6 +5,20 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Car myCar = new Car();
+            myCar.Details();
+            Car audi=new Car("Audi A4",250,"blue");
+            audi.Drive();
+            audi.Details();
+            Car bmw = new Car("BMW M5",350);
+            bmw.Drive();
+            bmw.Details();
+
+            string userInput=Console.ReadLine();
+            if(userInput == "1")
+            {
+                audi.Stop();
+            }
         }
     }
 }
